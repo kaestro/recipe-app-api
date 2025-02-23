ab9a22b6d674b71f98d5e36af3d0855be67540f9
kaestro <didme07@gmail.com>
2025-02-23 11:50:45 +0900

    implemented recipe images api

--- app/core/models.py
+++ app/core/models.py
@@ -9,6 +9,17 @@ from django.contrib.auth.models import (
     PermissionsMixin,
 )
 
+import os
+import uuid
+
+
+def recipe_image_file_path(instance, filename):
+    """Generate file path for new recipe image."""
+    ext = os.path.splitext(filename)[1]
+    filename = f'{uuid.uuid4()}{ext}'
+
+    return os.path.join('uploads', 'recipe', filename)
+
 
 class UserManager(BaseUserManager):
     """Manager for users."""
@@ -59,6 +70,7 @@ class Recipe(models.Model):
     link = models.CharField(max_length=255, blank=True)
     tags = models.ManyToManyField('Tag')
     ingredients = models.ManyToManyField('Ingredient')
+    image = models.ImageField(null=True, upload_to=recipe_image_file_path)
 
     def __str__(self):
         return self.title
